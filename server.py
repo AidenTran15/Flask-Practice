e5cee1d8151ffb02c98518fc044d618ab50d914b
AidenTran15 <aidenkiettran@gmail.com>
2020-02-22 17:38:04 +1100

    Change data to dict

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 
@@ -29,6 +29,12 @@ def contact():
 def home():
     return render_template('index.html')
 
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'Your form submitted'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return 'form submitted'
+    else:
+        return 'something went wrong. Try again!'
