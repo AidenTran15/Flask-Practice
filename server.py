ca6b6292c028c2cc73c79b9e516cb729e4657aa3
AidenTran15 <aidenkiettran@gmail.com>
2020-02-22 13:20:23 +1100

    [New] Upload css and js filefile

--- server.py
+++ server.py
@@ -7,11 +7,24 @@ def my_home():
     return render_template('index.html')
 
 
+
+# @app.route('/<string:page_name>')
+# def html_page(page_name):
+#     return render_template('page_name')
+
+
 @app.route('/about.html')
 def about():
     return render_template('about.html')
 
-
 @app.route('/works.html')
-def works():
+def work():
     return render_template('works.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
