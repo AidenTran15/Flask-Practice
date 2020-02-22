6bf092c82ec6337a3f442567dbd316fa8a6dee2e
AidenTran15 <aidenkiettran@gmail.com>
2020-02-23 10:03:12 +1100

    [Update]save data to database

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
@@ -29,12 +29,19 @@ def contact():
 def home():
     return render_template('index.html')
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data['message']
+        file = database.write(f"\n{email}, {subject},{message}")
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
-        return 'form submitted'
+        write_to_file(data)
+        return redirect('/thankyou.html')
     else:
         return 'something went wrong. Try again!'
